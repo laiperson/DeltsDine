35a8d5086122c3769a3c52170f06e11bc4d56408
Ben Wiley <wiley.ben77@gmail.com>
2019-12-04 15:46:01 -0600

    building out register and login functionality

--- app.py
+++ app.py
@@ -2,11 +2,15 @@
 # Imports
 #----------------------------------------------------------------------------#
 
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, flash, url_for, redirect, abort
+from flask_login import current_user, login_user, logout_user, LoginManager
+from flask_bcrypt import Bcrypt
+from django.utils.http import url_has_allowed_host_and_scheme
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.hybrid import hybrid_property
 from forms import RegisterForm, LoginForm, ForgotForm
 import os
 
@@ -20,6 +24,14 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Initialize Bcrypt for Hashing Password
+bcrypt = Bcrypt(app)
+
+# Configure Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# Configure DB Connection with SQLAlchemy
 Base = declarative_base()
 
 engine = create_engine(os.environ['DATABASE_URL'])
@@ -27,11 +39,14 @@ Session = sessionmaker(bind=engine)
 
 session = Session()
 
+# Get DB SQLAlchemy for Migration
+db = SQLAlchemy(app)
+
 # Automatically tear down SQLAlchemy.
 '''
 @app.teardown_request
 def shutdown_session(exception=None):
-    db_session.remove()
+    session.remove()
 '''
 
 # Login required decorator.
@@ -88,29 +103,71 @@ def add_meal():
         session.add(meal)
         session.commit()
 
-        return "Meal added with MealId={}".format(meal.MealId)
+        return "Meal added. {}".format(meal)
     except Exception as e:
         session.rollback()
         return "add_meal function returned error on adding meal with descr of {}. {}".format(description, str(e))
 
 @app.route('/meals/<id>/RSVPs')
 def get_meal_rsvps(id):
-    membersEmail = session.query(RSVP).filter(RSVP.MealId == id)
-    print("in get meal rsvps")
-    return membersEmail
+    membersEmail = session.query(RSVP).filter(RSVP.MealId == id).all()
+    return "in get meal rsvps. Got objects: {}".format(membersEmail)
 
 
 
 # ===== Authentication/User Identity Routes ===== #
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    
     form = LoginForm(request.form)
+
+    if form.validate_on_submit():
+        member = session.query(Member).filter(Member.Email == form.email.data).first()
+        if member is None or not member.check_password(form.password.data):
+            flash("Invalid username or password. Please try again.")
+            return redirect(url_for('login'))
+        
+        login_user(member)
+        flash('Logged in successfully')
+        next = request.args.get('next')
+        # is_safe_url should check if the url is safe for redirects.
+        # See http://flask.pocoo.org/snippets/62/ for an example.
+        if not url_has_allowed_host_and_scheme(next):
+            return abort(400)
+
+        return redirect(next or url_for('home'))
     return render_template('forms/login.html', form=form)
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
-@app.route('/register')
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
     form = RegisterForm(request.form)
+    if form.validate_on_submit():
+        member = Member(
+            Email = form.email.data, 
+            FirstName = form.firstName.data, 
+            LastName = form.lastName.data,
+            MealAllowance = form.mealAllowance.data,
+            WeekMealsUsed = form.WeekMealsUsed.data,
+            Active = True
+            # EmailConfirmed = False
+        )
+        member._set_password(form.password.data)
+        session.add(member)
+        session.commit()
+        flash('Congratulations! You have successfully registered for Delts Dine. Make sure to confirm your email.')
+        return redirect(url_for('home'))
+
     return render_template('forms/register.html', form=form)
 
 
@@ -123,7 +180,7 @@ def forgot():
 # Error handlers.
 @app.errorhandler(500)
 def internal_error(error):
-    #db_session.rollback()
+    session.rollback()
     return render_template('errors/500.html'), 500
 
 
--- models.py
+++ models.py
@@ -1,5 +1,8 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Table, Column, Integer, ForeignKey, String, Boolean, Date, DateTime
+from sqlalchemy.ext.hybrid import hybrid_property
+
+import app
 
 Base = declarative_base()
 
@@ -33,22 +36,36 @@ class Member(Base):
     __tablename__ = 'Member'
 
     Email = Column(String(length=16), primary_key=True)
+    _Password = Column(String(128))
     FirstName = Column(String())
     LastName = Column(String())
     MealAllowance = Column(Integer)
     WeekMealsUsed = Column(Integer)
     Active = Column(Boolean)
+    EmailConfirmed = Column(Boolean)
 
-    def __init(self, Email, FirstName, LastName, MealAllowance, WeekMealsUsed, Active):
+    def __init(self, Email, _Password, FirstName, LastName, MealAllowance, WeekMealsUsed, Active, EmailConfirmed):
         self.Email = Email
         self.FirstName = FirstName
         self.LastName = LastName
         self.MealAllowance = MealAllowance
         self.WeekMealsUsed = WeekMealsUsed
         self.Active = Active
+        self.EmailConfirmed = True          # Change this once email system is established
+
+    @hybrid_property
+    def password(self):
+        return self._Password
+
+    @password.setter
+    def _set_password(self, plainTextPassword):
+        self._Password = app.bcrypt.generate_password_hash(plainTextPassword)
+
+    def is_correct_password(self, plainTextPassword):
+        return app.bcrypt.check_password_hash(self.password(), plainTextPassword)
 
     def __repr__(self):
-        return "<Member(Email={}, FirstName={}, LastName={}, MealAllowance={}, WeekMealsUsed={}, Active={})>\n".format(self.Email, self.FirstName, self.LastName, self.MealAllowance, self.WeekMealsUsed, self.Active)
+        return "<Member(Email={}, FirstName={}, LastName={}, MealAllowance={}, WeekMealsUsed={}, Active={}, EmailConfirmed={})>\n".format(self.Email, self.FirstName, self.LastName, self.MealAllowance, self.WeekMealsUsed, self.Active, self.EmailConfirmed)
 
     def serialize(self):
         return {
@@ -57,7 +74,8 @@ class Member(Base):
             'LastName': self.LastName,
             'MealAllowance': self.MealAllowance,
             'WeekMealsUsed': self.WeekMealsUsed,
-            'Active': self.Active
+            'Active': self.Active, 
+            'EmailConfirmed': self.EmailConfirmed
         }
 
 
