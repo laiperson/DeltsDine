f4e688e82dfe2230fd33be85b6c8ddc11ace9ddc
Ben Wiley <wiley.ben77@gmail.com>
2019-12-10 10:35:15 -0600

    adding gmail api configuration and buildpack to help keep credentials in heroku config var

--- app.py
+++ app.py
@@ -48,27 +48,46 @@ db = SQLAlchemy(app)
 from models import Meal, Member, RSVP, CheckIn
 
 # Automatically tear down SQLAlchemy.
-'''
-@app.teardown_request
-def shutdown_session(exception=None):
-    session.remove()
-'''
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     session.close()
 
-# Login required decorator.
-'''
-def login_required(test):
-    @wraps(test)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return test(*args, **kwargs)
-        else:
-            flash('You need to login first.')
-            return redirect(url_for('login'))
-    return wrap
-'''
+# Gmail API Configuration
+Gmail_Scopes = ['https://www.googleapis.com/auth/gmail.send']
+"""Shows basic usage of the Gmail API.
+Lists the user's Gmail labels.
+"""
+creds = None
+# The file token.pickle stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+if os.path.exists('token.pickle'):
+    with open('token.pickle', 'rb') as token:
+        creds = pickle.load(token)
+# If there are no (valid) credentials available, let the user log in.
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'credentials.json', SCOPES)
+        creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open('token.pickle', 'wb') as token:
+        pickle.dump(creds, token)
+
+service = build('gmail', 'v1', credentials=creds)
+
+# Call the Gmail API
+results = service.users().labels().list(userId='me').execute()
+labels = results.get('labels', [])
+
+if not labels:
+    print('No labels found.')
+else:
+    print('Labels:')
+    for label in labels:
+        print(label['name'])
 
 
 
@@ -485,6 +504,47 @@ def internal_error(error):
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('errors/404.html'), 404
+
+
+#----------------------------------------------------------------------------#
+# Gmail API Controller.
+#----------------------------------------------------------------------------#
+@app.route('/Gmail/Send')
+def create_message(to_address, subject, message_text):
+    """Create a message for an email.
+
+    Args:
+        to: Email address of the receiver.
+        subject: The subject of the email message.
+        message_text: The text of the email message.
+
+    Returns:
+        An object containing a base64url encoded email object.
+    """
+    message = MIMEText(message_text)
+    message['to'] = to
+    message['from'] = "deltsdine@gmail.com"
+    message['subject'] = subject
+    return {'raw': base64.urlsafe_b64encode(message.as_string())}
+
+def send_message(service, message):
+    """Send an email message.
+
+    Args:
+        service: Authorized Gmail API service instance.
+        user_id: User's email address. The special value "me"
+        can be used to indicate the authenticated user.
+        message: Message to be sent.
+
+    Returns:
+        Sent Message.
+    """
+    try:
+        message = (service.users().messages().send(userId="deltsdine@gmail.com", body=message)
+                .execute())
+        return message
+    except errors.HttpError, error:
+        print 'An error occurred: %s' % error
 
 
 #----------------------------------------------------------------------------#
